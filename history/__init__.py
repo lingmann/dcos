986a150d85b3065a04968662fc6a8ad86167e16d
Matthias Veit <matthias_veit@yahoo.de>
2015-04-02 10:44:55 +0200

    add routes and health checks

--- history/__init__.py
+++ history/__init__.py
@@ -1,2 +1,2 @@
 __author__ = 'mesosphere'
-__version__ = "0.0.1"
+__version__ = "0.0.2"
--- history/server.py
+++ history/server.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, Response
+from flask import Flask, Response, redirect, url_for
 from history.statebuffer import StateBuffer
 from flask.ext.compress import Compress
 import logging
@@ -14,6 +14,14 @@ def hour():
     result = "[" +  ",".join(buffer.entries) + "]"
     headers = { "Access-Control-Allow-Origin": "*" }
     return Response(response=result, content_type="application/json", headers=headers)
+
+@app.route('/ping')
+def ping():
+    return "pong"
+
+@app.route('/')
+def home():
+    return redirect(url_for('hour'))
 
 def start(port = int(environ.get('PORT0', '5000')), urls = environ["MASTER_URLS"].split(",")):
     global buffer

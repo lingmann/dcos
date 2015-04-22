a8fe07a3afe6624340e9ff5977bd425362213ae5
Jason Swartz <dev@bks2.com>
2015-04-22 13:46:36 -0700

    added working storage acct; parameterized cluster name and num nodes

--- providers/azure/create.sh
+++ providers/azure/create.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -o nounset -o pipefail -o errexit
 
-azure group create "$1" "West US" -f azuredeploy.json -d "$1" -e azuredeploy-parameters.json
+if [ "$#" -ne 2 ]; then
+    echo "Usage: create.sh <cluster-name> <num-nodes>"
+    exit 1
+fi
+
+name=$1
+nodes=$2
+
+< azuredeploy-parameters.json tr '\n' ' '  | sed "s/_NAME_/$name/g;s/_NODES_/$nodes/" > /tmp/customparams.json
+azure group create "$name" "West US" -f azuredeploy.json -d "$name" -e /tmp/customparams.json
+
+echo "Created group $name"

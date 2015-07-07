40a55bc730afcf7bd058c31c447b0e410f8be395
Cody Maloney <cody@mesosphere.io>
2015-07-07 16:07:37 -0700

    aws: Use right region for s3 bucket

--- aws.py
+++ aws.py
@@ -35,7 +35,7 @@ if 'ENV_AWS_CONFIG' in os.environ:
     session_prod = boto3.session.Session(
         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-        region_name='us-west-2'
+        region_name='us-east-1'
         )
     session_dev = None
 else:

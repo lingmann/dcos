993bb110444a6a3edd0ed0ed01816a4221679461
Cody Maloney <cody@mesosphere.io>
2015-08-25 16:41:18 -0700

    Add a test for `mkpanda tree --mkbootstrap`.
    Currently fails because there are some files in the root that
    shouldn't be there (the systemd units)

--- pkgpanda/build/tests/test_build.py
+++ pkgpanda/build/tests/test_build.py
@@ -1,5 +1,9 @@
 from shutil import copytree
-from subprocess import check_call
+from subprocess import check_call, check_output
+
+
+def get_tar_contents(filename):
+    return set(check_output(["tar", "-tf", filename]).decode().splitlines())
 
 
 def package(resource_dir, name, tmpdir):
@@ -25,3 +29,84 @@ def test_url_extract(tmpdir):
 
 def test_no_buildinfo(tmpdir):
     package("resources/no_buildinfo", "no_buildinfo", tmpdir)
+
+
+def test_bootstrap(tmpdir):
+    pkg_dir = tmpdir.join("bootstrap_test")
+    copytree("resources/", str(pkg_dir))
+    with pkg_dir.as_cwd():
+        check_call(["mkpanda", "tree", "--mkbootstrap"])
+        bootstrap_id = open("bootstrap.latest", 'r').read().strip()
+        bootstrap_files = get_tar_contents(bootstrap_id + ".bootstrap.tar.xz")
+
+        # Seperate files that come from individual packages from those in the root directory
+        package_files = dict()
+        merged_files = set()
+        for path in bootstrap_files:
+            if not path.startswith("./packages/"):
+                merged_files.add(path)
+                continue
+
+            # Skip the packages folder itself
+            if path == './packages/':
+                continue
+
+            # Figure out the package name, file inside the package
+            path_parts = path.split('/')
+            package_name = path_parts[2].split('--')[0]
+            file_path = '/'.join(path_parts[3:])
+            file_set = package_files.get(package_name, set())
+
+            # don't add the package directory / empty path.
+            if len(file_path) == 0:
+                continue
+            file_set.add(file_path)
+            package_files[package_name] = file_set
+
+        # Check that the root has exactly the right set of files.
+        assert merged_files == set([
+            './',
+            './active.buildinfo.full.json',
+            './bootstrap',
+            './environment',
+            './environment.export',
+            './active/',
+            './active/base',
+            './active/url_extract',
+            './active/single_source',
+            './active/no_buildinfo',
+            './bin/',
+            './bin/mesos-master',
+            './bin/no_buildinfo',
+            './etc/',
+            './lib/',
+            './lib/libmesos.so',
+            './lib/no_buildinfo.so',
+            './include/',
+        ])
+        # Check that each package has just the right set of files. All the right packages are present.
+        assert package_files == {
+            'no_buildinfo': set([
+                'no_buildinfo',
+                'version',
+                'bin/no_buildinfo',
+                'buildinfo.full.json',
+                'pkginfo.json',
+                'lib/',
+                'bin/',
+                'lib/no_buildinfo.so']),
+            'url_extract': set(['pkginfo.json', 'buildinfo.full.json']),
+            'single_source': set(['pkginfo.json', 'buildinfo.full.json']),
+            'base': set([
+                'base',
+                'bin/',
+                'dcos.target.wants/',
+                'dcos.target.wants/dcos-foo.service',
+                'version',
+                'buildinfo.full.json',
+                'bin/mesos-master',
+                'pkginfo.json',
+                'lib/',
+                'lib/libmesos.so',
+                ])
+            }

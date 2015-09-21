3cefb49a3195b4d2316467721ff92e18f992e527
Jeremy Lingmann <jeremy@mesosphere.io>
2015-09-21 14:27:16 -0700

    Add cloud-config string validation
    Check for illegal cloud config characters before they are injected into
    the ARM template. Specifically, single quote characters are not allowed
    in an ARM template string.

--- gen/azure/providers_azure/gen_azure_custom_data.py
+++ gen/azure/providers_azure/gen_azure_custom_data.py
@@ -14,6 +14,18 @@ if len(sys.argv) != 2:
     print("Usage: {} CLOUD-CONFIG.yaml".format(sys.argv[0]))
     sys.exit(1)
 
+
+def validate_cloud_config(cc_string):
+    """Check for any invalid characters in the cloud config, and exit with an
+    error message if any invalid characters are detected."""
+    illegal_pattern = re.compile("[']")
+    illegal_match = illegal_pattern.search(cc_string)
+    if illegal_match:
+        print("ERROR: Illegal cloud config string detected.", file=sys.stderr)
+        print("ERROR: {} matches pattern {}".format(
+            illegal_match.string, illegal_match.re), file=sys.stderr)
+        sys.exit(1)
+
 with open(sys.argv[1]) as f:
     config = yaml.load(f)
 
@@ -24,8 +36,10 @@ print("[base64(concat('#cloud-config\n\n', ", end='')
 prevend = 0
 for m in re.finditer('(?P<pre>.*?)\[\[\[(?P<inject>.*?)\]\]\]', j):
     before = m.group('pre')
+    validate_cloud_config(before)
     param = m.group('inject')
     print("'{}', {},".format(before, param), end='')
     prevend = m.end()
 
+validate_cloud_config(j[prevend:])
 print("'{}'))]".format(j[prevend:]), end='')

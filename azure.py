21537931ed93c6905120f11b861b3f10e772526b
Cody Maloney <cody@mesosphere.io>
2015-09-23 13:54:22 -0700

    WIP

--- azure.py
+++ azure.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Azure Image Creation, Management, Testing"""
 
-import argparse
 import json
 import re
 import sys
@@ -11,33 +10,13 @@ from copy import deepcopy
 import gen
 import util
 
+# TODO(cmaloney): Make it so the template only completes when services are properly up.
+late_services = ""
 
-late_services = """- name: dcos-cfn-signal.service
-  command: start
-  content: |
-    [Unit]
-    Description=Signal CloudFormation Success
-    After=dcos.target
-    Requires=dcos.target
-    ConditionPathExists=!/var/lib/dcos-cfn-signal
-    [Service]
-    Type=simple
-    Restart=on-failure
-    StartLimitInterval=0
-    RestartSec=15s
-    ExecStartPre=/usr/bin/docker pull mbabineau/cfn-bootstrap
-    ExecStartPre=/bin/ping -c1 leader.mesos
-    ExecStartPre=/usr/bin/docker run --rm mbabineau/cfn-bootstrap \\
-      cfn-signal -e 0 \\
-      --resource {{ report_name }} \\
-      --stack { "Ref": "AWS::StackName" } \\
-      --region { "Ref" : "AWS::Region" }
-    ExecStart=/usr/bin/touch /var/lib/dcos-cfn-signal"""
-
-cf_instance_groups = {
+instance_groups = {
     'master': {
         'report_name': 'MasterServerGroup',
-        'roles': ['master', 'aws_master']
+        'roles': ['master', 'azure_master']
     },
     'slave': {
         'report_name': 'SlaveServerGroup',
@@ -45,10 +24,9 @@ cf_instance_groups = {
     }
 }
 
-AWS_REF_REGEX = re.compile(r"(?P<before>.*)(?P<ref>{ .* })(?P<after>.*)")
 
-
-# TODO(mj): Use RPC if Azure supports it
+# Validate that there aren't any single quotes present since they break the ARM
+# template system.
 def validate_cloud_config(cc_string):
     """Check for any invalid characters in the cloud config, and exit with an
     error message if any invalid characters are detected."""
@@ -61,54 +39,61 @@ def validate_cloud_config(cc_string):
         sys.exit(1)
 
 
-# TODO(mj): update, add return
-def transform(line):
+# Transforms the given yaml to be a list of strings which are concatenated
+# together by the ARM template system. We must make it a list of strings so that
+# ARM template parameters appear at the top level of the template and get substituted
+def transform(cloud_config_yaml_str):
+    cc_json = json.dumps(yaml.load(cloud_config_yaml_str), sort_keys=True)
+    arm_list_str = "[base64(concat('#cloud-config\n\n', "
+    # Find template parameters and seperate them out as seperate elements in a
+    # json list.
     prevend = 0
-    for m in re.finditer('(?P<pre>.*?)\[\[\[(?P<inject>.*?)\]\]\]', j):
+    for m in re.finditer('(?P<pre>.*?)\[\[\[(?P<inject>.*?)\]\]\]', cc_json):
         before = m.group('pre')
-        validate_cloud_config(before)
         param = m.group('inject')
-        print("'{}', {},".format(before, param), end='')
+        validate_cloud_config(before)
+        arm_list_str += "'{}', {},".format(before, param)
         prevend = m.end()
 
-    print("[base64(concat('#cloud-config\n\n', ", end='')
-    validate_cloud_config(j[prevend:])
-    print("'{}'))]".format(j[prevend:]), end='')
+    # Add the last little bit
+    validate_cloud_config(cc_json[prevend:])
+    arm_list_str += "'{}'))]".format(cc_json[prevend:])
+
+    # Return the generated string
+    return arm_list_str
 
 
-def render_cloudformation(
-        # default CF template with some preset parameters already (calc.py) replaced
-        cf_template,
-        master_cloudconfig,
-        slave_cloudconfig):
-    # TODO(cmaloney): There has to be a cleaner way to do this transformation.
-    # For now just moved from cloud_config_cf.py
-    # TODO(cmaloney): Move with the logic that does this same thing in Azure
-    def transform_lines(text):
-        return ''.join(map(transform, text.splitlines())).rstrip(',\n')
+def render_arm(
+        arm_template_json_str,
+        master_cloudconfig_yaml_str,
+        slave_cloudconfig_yaml_str):
 
-    print(cf_template)
-    template_str = util.jinja_env.from_string(cf_template).render({
-        'master_cloud_config': transform_lines(master_cloudconfig),
-        'slave_cloud_config': transform_lines(slave_cloudconfig)
+    template_str = util.jinja_env.from_string(arm_template_json_str).render({
+        'master_cloud_config': transform(master_cloudconfig_yaml_str),
+        'slave_cloud_config': transform(slave_cloudconfig_yaml_str)
     })
 
-    print(template_str)
+    # Add in some metadata to help support engineers
     template_json = json.loads(template_str)
-
-    template_json['Metadata']['DcosImageCommit'] = util.dcos_image_commit
-    template_json['Metadata']['TemplateGenerationDate'] = util.template_generation_date
-
+    template_json['variables']['DcosImageCommit'] = util.dcos_image_commit
+    template_json['variables']['TemplateGenerationDate'] = util.template_generation_date
     return json.dumps(template_json)
 
 
+# Render the cloud_config template given a particular set of options
+#
+# Arguments:
+#   arguments: a python dictionary of arguments to pass to the gen library. They
+#     are user input arguments which get filled in / prompted for
+#   options: Options to control the gen library itself, such as whether to save
+#     the user config, the final generated config, if it is non-interactive
 def gen_templates(arguments, options):
     results = gen.generate(
         options=options,
-        # mounting drive, disable etcd
-        mixins=['aws', 'coreos', 'coreos-aws'],
+        # Mixins which add mounting the drive, disabling etcd, provider config
+        # around how to access the master load balancer, etc.
+        mixins=['azure', 'coreos'],
         # jinja template
-        extra_templates={'cloudformation': ['gen/aws/templates/cloudformation.json']},
         arguments=arguments
         )
 
@@ -119,31 +104,33 @@ def gen_templates(arguments, options):
 
     # Specialize for master, slave, slave_public
     variant_cloudconfig = {}
-    for variant, params in cf_instance_groups.items():
+    for variant, params in instance_groups.items():
         cc_variant = deepcopy(cloud_config)
 
-        # Specialize the dcos-cfn-signal service
-        cc_variant = results.utils.add_units(
-            cc_variant,
-            yaml.load(util.jinja_env.from_string(late_services).render(params)))
+        # TODO(cmaloney): Add the dcos-arm-signal service here
+        # cc_variant = results.utils.add_units(
+        #     cc_variant,
+        #     yaml.load(util.jinja_env.from_string(late_services).render(params)))
 
         # Add roles
-        cc_variant = results.utils.add_roles(cc_variant, params['roles'] + ['aws'])
+        cc_variant = results.utils.add_roles(cc_variant, params['roles'] + ['azure'])
 
-        # NOTE: If this gets printed in string stylerather than '|' the AWS
-        # parameters which need to be split out for the cloudformation to
+        # NOTE: If this gets printed in string stylerather than '|' the Azure
+        # parameters which need to be split out for the arm to
         # interpret end up all escaped and undoing it would be hard.
         variant_cloudconfig[variant] = results.utils.render_cloudconfig(cc_variant)
 
-    # Render the cloudformation
-    cloudformation = render_cloudformation(
-        results.templates['cloudformation'],
+    # Render the arm
+    arm = render_arm(
+        open('gen/azure/azuredeploy.json').read(),
         variant_cloudconfig['master'],
         variant_cloudconfig['slave']
         )
 
+    # TODO(mj): Use RPC against azure to validate the ARM template is well-formed
+
     return gen.Bunch({
-        'cloudformation': cloudformation,
+        'arm': arm,
         'results': results
     })
 
@@ -153,12 +140,8 @@ def do_create(tag, channel, commit, gen_arguments):
     gen_options = gen.get_options_object()
     gen_arguments['master_discovery'] = 'cloud_dynamic'
     single_args = deepcopy(gen_arguments)
-    multi_args = deepcopy(gen_arguments)
     single_args['num_masters'] = 1
-    multi_args['num_masters'] = 3
     single_master = gen_templates(single_args, gen_options)
-    multi_master = gen_templates(multi_args, gen_options)
-    button_page = gen_buttons(channel, tag, commit)
 
     # Make sure we upload the packages for both the multi-master templates as well
     # as the single-master templates.
@@ -169,10 +152,10 @@ def do_create(tag, channel, commit, gen_arguments):
         'extra_packages': extra_packages,
         'files': [
             {
-                'known_path': 'azure/single-master.cloudformation.json',
-                'stable_path': 'azure/{}.single-master.cloudformation.json'.format(
+                'known_path': 'azure/single-master.azuredeploy.json',
+                'stable_path': 'azure/{}.single-master.azuredeploy.json'.format(
                     single_master.results.arguments['config_id']),
-                'content': single_master.cloudformation
+                'content': single_master.arm
             }
         ]
     }

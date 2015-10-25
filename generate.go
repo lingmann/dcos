b6d0a606d4a856167ff97057b4eda5b9c897224d
malnick <malnick@gmail..com>
2015-10-25 09:29:05 -0700

    template

---
+++ generate.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"os"
+	//"text/template"
+)
+
+func generate(config Config, gentype string) {
+	log.Info("Generating configuration for ", config.ClusterName, " in ", config.OutputDir, " for installation type ", gentype)
+	switch gentype {
+	case "onprem":
+		do_onprem(config)
+	case "aws":
+	case "chef":
+	default:
+		log.Error(gentype, " is not a supported installation type. Exiting")
+		os.Exit(1)
+	}
+}
+
+func do_onprem(config Config) {
+	log.Info("Starting on premise configuration generation...")
+
+}

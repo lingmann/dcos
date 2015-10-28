cc5253ddf4b3b4ccb668b0563b09e6bcd4dfc3dc
malnick <malnick@gmail..com>
2015-10-28 09:53:21 -0700

    stuff

--- dcos_installer/server.py
+++ dcos_installer/server.py
@@ -1,9 +1,8 @@
-from flask import Flask
-from flask import render_template
 from fabric.api import run
+from flask import Flask
+from flask import request
+from flask import render_template
 import logging as log
-
-
 
 def run(options):
     """
@@ -12,13 +11,26 @@ def run(options):
     in the future.
     """
     log.info("Executing Flask server...")
+    # Define a new Flask object
     app = Flask(__name__)
+    # Create our app URIs
     do_routes(app,options)
+    # Define the Flask log level if we set it un CLI flags
+    if options.log_level == 'debug':
+        app.debug = True
+
     app.run()
 
 def do_routes(app,options):
-    @app.route('/installer/v1.0/')
+    """
+    Organize all our routes into a single def so we can keep
+    all our routes defined in one place.
+    """
+    version = '1.0'
+    @app.route("/installer/v{}/".format(version), methods=['POST', 'GET'])
     def mainpage():
+        if request.method == 'POST':
+            
         return render_template('main.html', title='Flask Test')
 
 

59e1ea10c9b7958ac679b2c58988dcf0ff6b42ba
malnick <malnick@gmail..com>
2015-11-19 16:05:51 -0800

    comments

--- dcos_installer/preflight.py
+++ dcos_installer/preflight.py
@@ -11,15 +11,21 @@ def check(options):
     """
     SSH via SubProcess Calls... 
     """
+    # Set our SSH user, already validated.
     ssh_user = open(options.ssh_user_path, 'r').read().lstrip().rstrip()
 
+    # Get a remote cmd object and set it up to execute the preflight script
     preflight = DCOSRemoteCmd()
     preflight.ssh_user = ssh_user 
     preflight.ssh_key_path = options.ssh_key_path
     preflight.inventory_path = options.hosts_yaml_path
     preflight.log_directory = options.log_directory
     preflight.command = 'sudo bash /home/{}/install_dcos.sh --preflight-only'.format(ssh_user)
+
+    # Validate our inputs for the object are ok
     no_errors = preflight.validate()
+
+    # If we got errors, show them; if not, execute preflight
     if not no_errors:
         log.error("Could not execute preflight, errors encountered during validation.", err)
         for key, value in no_errors.items():

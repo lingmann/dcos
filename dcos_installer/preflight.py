f736ca46509eeae6b72baf924e49943ed7898c61
malnick <malnick@gmail..com>
2015-11-20 11:15:16 -0800

    pushing to masster

--- dcos_installer/preflight.py
+++ dcos_installer/preflight.py
@@ -1,8 +1,5 @@
-import paramiko
-import multiprocessing as mp
-from multiprocessing import Process
 #from dcos_installer.ssh.execute import DCOSRemoteCmd
-from ssh.execute import DCOSRemoteCmd
+from ssh.remote_cmd import RemoteCmd
 from dcos_installer.log import DCOSLog
 log = DCOSLog(__name__).log
 
@@ -16,7 +13,7 @@ def check(options):
     ssh_user = open(options.ssh_user_path, 'r').read().lstrip().rstrip()
 
     # Get a remote cmd object and set it up to execute the preflight script
-    preflight = DCOSRemoteCmd()
+    preflight = RemoteCmd()
     preflight.ssh_user = ssh_user 
     preflight.ssh_key_path = options.ssh_key_path
     preflight.inventory_path = options.hosts_yaml_path
@@ -24,14 +21,14 @@ def check(options):
     preflight.command = 'sudo bash /home/{}/install_dcos.sh --preflight-only'.format(ssh_user)
 
     # Validate our inputs for the object are ok
-    no_errors = preflight.validate()
+    errors = preflight.validate()
 
     # If we got errors, show them; if not, execute preflight
-    if not no_errors:
-        log.error("Could not execute preflight, errors encountered during validation.", err)
-        for key, value in no_errors.items():
+    if len(errors) > 0:
+        log.error("Could not execute preflight, errors encountered during validation.")
+        for key, value in errors.items():
             log.error("%s: %s", key, value)
-        return no_errors 
+        return errors 
 
     else:
         preflight.execute()

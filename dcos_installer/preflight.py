6f776c138e4ee94e3b1a9779fe4d530dbbe559a9
malnick <malnick@gmail..com>
2015-11-26 08:41:16 -0800

    everything but copy

--- dcos_installer/preflight.py
+++ dcos_installer/preflight.py
@@ -10,44 +10,45 @@ def check(options, config):
     """
     # Get a remote cmd object and set it up to execute the preflight script for masters
     log.info("Executing Preflight on Masters...")
-    preflight = RemoteCmd()
-    preflight.ssh_user = config['ssh_user'] 
-    preflight.ssh_key_path = config['ssh_key_path']
-    preflight.inventory = config['master_list']
-    preflight.log_directory = options.log_directory
-    preflight.command = 'sudo bash /home/{}/install_dcos.sh --preflight-only'.format(config['ssh_user'])
+    preflight_masters = RemoteCmd()
+    preflight_masters.ssh_user = config['ssh_user'] 
+    preflight_masters.ssh_key_path = config['ssh_key_path']
+    preflight_masters.inventory = config['master_list']
+    preflight_masters.log_directory = options.log_directory
+    preflight_masters.command = 'sudo bash /home/{}/install_dcos.sh --preflight-only'.format(config['ssh_user'])
     # Validate our inputs for the object are ok
-    errors = preflight.validate()
-
-    # If we got errors, show them; if not, execute preflight
-    if len(errors) > 0:
-        log.error("Could not execute preflight on masters, errors encountered during validation.")
-        for key, value in errors.items():
-            log.error("%s: %s", key, value)
-        return errors 
-    else:
-        preflight.execute()
-        return False
+    errors_masters = preflight_masters.validate()
 
     # Run it again on slaves...
     log.info("Executing Preflight on Slaves...")
-    preflight = RemoteCmd()
-    preflight.ssh_user = config['ssh_user'] 
-    preflight.ssh_key_path = config['ssh_key_path']
-    preflight.inventory = config['agent_list']
-    preflight.log_directory = options.log_directory
-    preflight.command = 'sudo bash /home/{}/install_dcos.sh --preflight-only'.format(config['ssh_user'])
+    preflight_agents = RemoteCmd()
+    preflight_agents.ssh_user = config['ssh_user'] 
+    preflight_agents.ssh_key_path = config['ssh_key_path']
+    preflight_agents.inventory = config['agent_list']
+    preflight_agents.log_directory = options.log_directory
+    preflight_agents.command = 'sudo bash /home/{}/install_dcos.sh --preflight-only'.format(config['ssh_user'])
     # Validate our inputs for the object are ok
-    errors = preflight.validate()
+    errors_agents = preflight_agents.validate()
 
     # If we got errors, show them; if not, execute preflight
-    if len(errors) > 0:
-        log.error("Could not execute preflight on agents, errors encountered during validation.")
-        for key, value in errors.items():
-            log.error("%s: %s", key, value)
-        return errors 
+    if len(errors_masters) > 0 or len(errors_agents) > 0:
+        log.error("Could not execute preflight, errors encountered during validation.")
+        return_errors = {}
+        if errors_masters:
+            for key, value in errors_masters.items():
+                log.error("%s: %s", key, value)
+                return_errors[key] = value
+
+        if errors_agents:
+            for key, value in errors_agents.items():
+                log.error("%s: %s", key, value)
+                return_errors[key] = value
+
+        return return_errors
+
     else:
-        preflight.execute()
+        preflight_masters.execute()
+        preflight_agents.execute()
         return False
 
 

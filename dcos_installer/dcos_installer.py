df1c490a793d629fab05e64b1c535b777833db98
malnick <malnick@gmail..com>
2015-12-29 14:46:12 -0800

    cleaned up installer entrypoint/cli args

--- dcos_installer/dcos_installer.py
+++ dcos_installer/dcos_installer.py
@@ -1,17 +1,12 @@
 import argparse
 import logging
-import os
 import sys
 
-from dcos_installer import pretty_log
-#from dcos_installer.server import app
-
-
-# Get the current relative directory
-project_path=os.path.dirname(os.path.realpath(__file__))
+from dcos_installer import server, pretty_log
 
 # Get logger
 log = logging.getLogger(__name__)
+
 
 class DcosInstaller:
     def __init__(self, options=None):
@@ -21,137 +16,64 @@ class DcosInstaller:
         site-based installation of DCOS.
         """
         if not options:
-            options=self.parse_args(sys.argv[1:])
+            options = self.parse_args(sys.argv[1:])
         self.set_log_level(options)
-        self.set_install_dir(options)
 
-        if options.mode == 'web':
-            import dcos_installer.server.app
-            #app.run(options)
-        else:
-            log.error("Sorry, %s is not a usable run mode.", options.mode)
-            sys.exit(1)
-
-    def set_install_dir(self, options):
-        """
-        Ensures the default or user provided install directory path
-        exists.
-        """
-        try:
-            os.stat(options.install_directory)
-        except:
-            log.info('{} does not exist, creating.'.format(options.install_directory))
-            os.mkdir(options.install_directory)
-        # Logging directory
-        try:
-            os.stat(options.log_directory)
-        except:
-            log.info('{} does not exist, creating.'.format(options.log_directory))
-            os.mkdir(options.log_directory)
-        # Serve directory
-        try:
-            os.stat(options.serve_directory)
-        except:
-            log.info('{} does not exist, creating.'.format(options.serve_directory))
-            os.mkdir(options.serve_directory)
-
+        if options.web:
+            server.start(options)
 
     def set_log_level(self, options):
         """
         Given a map of options, parse for log level flag and set the
         default logging level.
         """
-        log.log_level = options.log_level
+        if options.verbose:
+            log.setLevel(logging.DEBUG)
+            log.debug("Running with verbose logger")
 
-    def parse_args(self,args):
+    def parse_args(self, args):
         """
         Parse CLI arguments and return a map of options.
         """
-        # Get the user's homedir in a cross platfrom manner
-        # Can set top level install dir as env var or accept default in $HOME
-        homedir    = os.path.expanduser('~')
-        if 'DCOS_INSTALL_DIR' in os.environ:
-            installdir = os.environ['DCOS_INSTALL_DIR']
-        else:
-            installdir = '{}/dcos-installer'.format(homedir)
-            
         parser = argparse.ArgumentParser(description='Install DCOS on-premise')
-        parser.add_argument(
-            '--log-directory',
-            type=str,
-            default='{}/logs'.format(installdir),
-            help='Path to the logging directory. Used for preflight.log and deploy.log..')
+        mutual_exc = parser.add_mutually_exclusive_group()
 
         parser.add_argument(
-            '--dcos-install-script-path',
-            type=str,
-            default='{}/dcos_installer/templates/install_dcos.sh'.format(project_path),
-            help='The path to install_dcos.sh script. Defaults to sibling of the project repo.')
-
-        parser.add_argument(
-            '--ip-detect-path',
-            type=str,
-            default='{}/ip-detect'.format(installdir),
-            help='The path to the ip-detect script.')
-
-        parser.add_argument(
-            '-c',
-            '--config-path',
-            type=str,
-            default='{}/dcos_config.yaml'.format(installdir),
-            help='The path to dcos_config.yaml.')
-
-        parser.add_argument(
-            '-d',
-            '--deploy',
+            '-v',
+            '--verbose',
             action='store_true',
             default=False,
-            help='Execute a deploy. Assumes preflight checks have already been executed and the cluster is in a usable state.')
+            help='Verbose log output (DEBUG).')
 
         parser.add_argument(
-            '-i',
-            '--install-directory',
-            type=str,
-            default=installdir,
-            help='The install directory for the DCOS installer.')
-
-        parser.add_argument(
-            '-l',
-            '--log-level',
-            type=str,
-            default='info',
-            choices=['info','debug'],
-            help='Log level.')
-
-        parser.add_argument(
-            '-m',
-            '--mode',
-            type=str,
-            choices=['cli', 'web'],
-            default='web',
-            help='Installation mode.')
-
-        parser.add_argument(
-            '-p', 
+            '-p',
             '--port',
             type=int,
             default=9000,
             help='Web server port number.')
 
-        parser.add_argument(
-            '--serve-directory',
-            type=str,
-            default='{}/serve'.format(installdir),
-            help='The path to the serve directory. Used to dump generated config and packages.')
+        mutual_exc.add_argument(
+            '-w',
+            '--web',
+            action='store_true',
+            default=True,
+            help='Run the web interface.')
 
-        parser.add_argument(
+        mutual_exc.add_argument(
             '-pre',
             '--preflight',
             action='store_true',
             default=False,
-            help='Execute the preflight checks on a series of nodes. Assumes $INSTALL_DIR/hosts.yaml exists.')
+            help='Execute the preflight checks on a series of nodes.')
 
-        parser.add_argument(
+        mutual_exc.add_argument(
+            '-d',
+            '--deploy',
+            action='store_true',
+            default=False,
+            help='Execute a deploy.')
+
+        mutual_exc.add_argument(
             '-t',
             '--test',
             action='store_true',
--- dcos_installer/server.py
+++ dcos_installer/server.py
@@ -24,67 +24,65 @@ version = '1'
 action_names = ['deploy', 'preflight', 'postflight']
 
 
-@app.route('/', methods=['GET'])
-def root():
-    log.info("Root page requested")
-    return redirect(url_for('main'))
+def start(options):
+    @app.route('/', methods=['GET'])
+    def root():
+        log.info("Root page requested")
+        return redirect(url_for('main'))
 
 
-@app.route('/api/v{}'.format(version), methods=['GET'])
-def main():
-    log.info("Main page requested")
-    return render_template('index.html')
+    @app.route('/api/v{}'.format(version), methods=['GET'])
+    def main():
+        log.info("Main page requested")
+        return render_template('index.html')
 
 
-@app.route('/api/v{}/configure'.format(version), methods=['GET', 'POST'])
-def configure():
-    if request.method == 'POST':
-        new_config = request.json
-        log.info('POST to configure: {}'.format(new_config))
-        _, config = mock.validate(new_config)
-        resp = Response(json.dumps(config))
-        resp.headers['Content-Type'] = 'application/json'
-        return resp
+    @app.route('/api/v{}/configure'.format(version), methods=['GET', 'POST'])
+    def configure():
+        if request.method == 'POST':
+            new_config = request.json
+            log.info('POST to configure: {}'.format(new_config))
+            _, config = mock.validate(new_config)
+            resp = Response(json.dumps(config))
+            resp.headers['Content-Type'] = 'application/json'
+            return resp
 
-    elif request.method == 'GET':
-        _, config = mock.validate()
-        resp = Response(json.dumps(config))
-        resp.headers['Content-Type'] = 'application/json'
-        return resp
+        elif request.method == 'GET':
+            _, config = mock.validate()
+            resp = Response(json.dumps(config))
+            resp.headers['Content-Type'] = 'application/json'
+            return resp
 
 
-@app.route('/api/v{}/<action_name>'.format(version), methods=['GET', 'POST'])
-def do_action_name(action_name):
-    if action_name in action_names:
-        if request.method == 'GET':
-            log.info('GET {}'.format(action_name))
-            # Do nothing
-        elif request.method == 'POST':
-            log.info('POST {}'.format(action_name))
-            # Execute deploy wrapper
+    @app.route('/api/v{}/<action_name>'.format(version), methods=['GET', 'POST'])
+    def do_action_name(action_name):
+        if action_name in action_names:
+            if request.method == 'GET':
+                log.info('GET {}'.format(action_name))
+                # Do nothing
+            elif request.method == 'POST':
+                log.info('POST {}'.format(action_name))
+                # Execute deploy wrapper
 
-        return json.dumps(mock.mock_action_state)
+            return json.dumps(mock.mock_action_state)
 
-        # TODO(malnick)
-        # Flask handles returning the 405 for method not allowed for us,
-        # unsure if we want to implement our own internal exception when
-        # a method not allowed is used?
+            # TODO(malnick)
+            # Flask handles returning the 405 for method not allowed for us,
+            # unsure if we want to implement our own internal exception when
+            # a method not allowed is used?
 
-    else:
-        log.error('Action not supported. Supported actions: {}'.format(action_names))
+        else:
+            log.error('Action not supported. Supported actions: {}'.format(action_names))
+            return render_template('404.html'), 404
+
+
+    @app.route('/api/v{}/success'.format(version), methods=['GET'])
+    def success():
+        return json.dumps(mock.mock_success())
+
+
+    @app.errorhandler(404)
+    def page_not_found(e):
         return render_template('404.html'), 404
 
-
-@app.route('/api/v{}/success'.format(version), methods=['GET'])
-def success():
-    return json.dumps(mock.mock_success())
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-app.run()
-
-#if __name__ == '__main__':
-#    app.run()
+    app.run()

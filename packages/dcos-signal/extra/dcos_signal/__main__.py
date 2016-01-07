85399949c28dbbcc071d3252cbf909eed7fa87c2
Cody Maloney <cmaloney@theoreticalchaos.com>
2016-01-06 17:19:15 -0800

    Move things into __init__.py to reduce number of almost empty files

--- packages/dcos-signal/extra/dcos_signal/__main__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from dcos_signal.dcos_signal import start
-
-if __name__ == '__main__':
-    start()
--- packages/dcos-signal/extra/setup.py
+++ packages/dcos-signal/extra/setup.py
@@ -1,9 +1,8 @@
-import dcos_signal
 from setuptools import setup
 
 config = {
     'name': 'dcos-signal',
-    'version': dcos_signal.__version__,
+    'version': "0.0.9",
     'description': 'Sends cluster information to segmentio',
     'author': 'Michael Jin',
     'author_email': 'michael@mesosphere.io',
@@ -12,7 +11,7 @@ config = {
     'url': 'https://github.com/mesosphere/dcos-signal',
     'packages': ['dcos_signal'],
     'entry_points': {
-        'console_scripts': ['dcos-signal = dcos_signal.__main__:start']
+        'console_scripts': ['dcos-signal = dcos_signal:main']
     },
     'install_requires': [
         "analytics-python",

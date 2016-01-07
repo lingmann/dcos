3989bb89a94426d27aa213aa5a98fcec5158a8ee
Cody Maloney <cmaloney@theoreticalchaos.com>
2016-01-06 16:08:59 -0800

    Swith dcos-signal to be built out of "extra" rather than an external repository
    Also fix include ordering, indentation issues

--- packages/dcos-signal/extra/setup.py
+++ packages/dcos-signal/extra/setup.py
@@ -1,27 +1,23 @@
 import dcos_signal
+from setuptools import setup
 
 config = {
-  'name': 'dcos-signal',
-  'version': dcos_signal.__version__,
-  'description': 'Sends cluster information to segmentio',
-  'author': 'Michael Jin',
-  'author_email': 'michael@mesosphere.io',
-  'maintainer': 'Mesosphere',
-  'maintainer_email': 'support@mesosphere.io',
-  'url': 'https://github.com/mesosphere/dcos-signal',
-  'packages': [
-    'dcos_signal'
-  ],
-  'entry_points': {
-    'console_scripts': [
-      'dcos-signal = dcos_signal.__main__:start'
+    'name': 'dcos-signal',
+    'version': dcos_signal.__version__,
+    'description': 'Sends cluster information to segmentio',
+    'author': 'Michael Jin',
+    'author_email': 'michael@mesosphere.io',
+    'maintainer': 'Mesosphere',
+    'maintainer_email': 'support@mesosphere.io',
+    'url': 'https://github.com/mesosphere/dcos-signal',
+    'packages': ['dcos_signal'],
+    'entry_points': {
+        'console_scripts': ['dcos-signal = dcos_signal.__main__:start']
+    },
+    'install_requires': [
+        "analytics-python",
+        "docopt"
     ]
-  },
-  'install_requires': [
-    "analytics-python",
-    "docopt"
-  ]
 }
 
-from setuptools import setup
 setup(**config)

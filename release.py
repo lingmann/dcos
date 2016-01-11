5d1b65bfca9efbe0aa436b75c37abcf61c869b3e
Michael Ellenburg <michael.ellenburg@mesosphere.com>
2016-01-10 22:07:35 -0800

    Packaging
    -Add shell script template
    -Add dockerfile template
    -use environ to get dcos_image git info
    -run tox inside docker container

--- release.py
+++ release.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
 """
-Create DCOS installer executable
-
-Usage:
-  release ( create | test )
-
-Options:
-  -h --help    Show this screen
-  --version    Show version
+Creates docker image run environment for installer and
+packages it into a bash script
 """
 import argparse
+import os
 import subprocess
 
-from docopt import docopt
+GIT_SHA = os.getenv("GIT_SHA", None)
+DCOS_IMAGE_COMMIT = os.getenv("DCOS_IMAGE_COMMIT", None)
 
-try:
-    GIT_SHA = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().replace("\n","")
-except:
-    print("Unable to parse git revision! Git must be installed.")
-    exit(1)
+def get_git_commit():
+    try:
+        return subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().replace("\n","")
+    except:
+        print("Unable to parse git revision! Git must be installed.")
+        exit(1)
 
 def write_string(filename, data):
     with open(filename, "w+") as f:
@@ -31,14 +28,19 @@ def load_string(filename):
 def do_create():
     # make docker
     docker_image_name = "dcos_installer:{}".format(GIT_SHA)
+    # Write Dockerfile
+    dockerfile_contents = load_string('Dockerfile.in').format(DCOS_IMAGE_COMMIT=DCOS_IMAGE_COMMIT)
+    print("Writing Dockerfile with the following contents:\n{}".format(dockerfile_contents))
+    write_string('Dockerfile', dockerfile_contents)
+    # Build Docker image
     subprocess.check_call(['docker', 'build', '-t', docker_image_name, "."])
-    installer_filename = "dcos_installer.sh"
-    installer_tar = "docker_installer_{}.tar".format(GIT_SHA)
     # Export docker image
+    installer_tar = "dcos_installer_{}.tar".format(GIT_SHA)
     subprocess.check_call(
         ['docker', 'save', docker_image_name],
         stdout=open(installer_tar, 'w'))
     # Write script to file
+    installer_filename = "dcos_installer.sh"
     write_string(
         installer_filename,
         load_string('dcos_installer.sh.in').format(installer_tar=installer_tar, docker_image_name=docker_image_name) + '\n#EOF#\n')
@@ -47,15 +49,20 @@ def do_create():
     # Make script executbale
     subprocess.check_call(['chmod', '+x', installer_filename])
 
-def do_test():
-    pass
+def parse_args():
+    parser = argparse.ArgumentParser(description="Creates installer as dcos_installer.sh")
+    parser.add_argument("--create", action="store_true", help="Create the installer script")
+    parser.add_argument("--version", action="store_true", help="Print the git SHA of this revision")
+    return parser.parse_args()
 
-def main(arguments):
-    if arguments["create"] is True:
+def main(args):
+    if args.create:
         do_create()
-    elif arguments["test"] is True:
-        do_test()
+    elif args.version:
+        print(GIT_SHA)
+    else:
+        exit(0)
 
 if __name__=='__main__':
-    arguments = docopt(__doc__, version=GIT_SHA)
+    arguments = parse_args()
     main(arguments)

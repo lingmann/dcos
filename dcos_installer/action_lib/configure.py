c1972502c98ab0fd95b112b952059a6fb6c8eb86
malnick <malnick@gmail..com>
2016-01-27 15:48:36 -0800

    appears to be working

--- dcos_installer/action_lib/configure.py
+++ dcos_installer/action_lib/configure.py
@@ -6,65 +6,12 @@ import sys
 import gen
 import pkgpanda
 import providers.bash as bash
-import yaml
 from dcos_installer.util import IP_DETECT_PATH, SERVE_DIR
-
-mock_config = yaml.load("""
----
-# Cluster configuration for DCOS
-# This is the local path on the target machine where the installer dumps the
-# packages and bootstrap tar ball.
-bootstrap_url: 'file:///opt/dcos_install_tmp'
-
-# The name of the cluster, we default to the following:
-cluster_name: 'Mesosphere: The Data Center Operating System'
-
-# Docker GC defaults, recommended to leave as default unless you're advanced.
-#docker_remove_delay: '1hrs'
-
-# The default backend for Exhibitor is Zookeeper. Advanced options are
-# shared_fs and aws_s3
-exhibitor_storage_backend: zookeeper
-
-# If you use the default Zk backend for Exhibitor, you need to pass in
-# the IPs of the Zk cluster for it to use. This is a COMMA SEPARATED LIST,
-# not to be confused with an actual array. You must also pass the :$PORT.
-exhibitor_zk_hosts: 10.33.2.20:2181
-
-# This is an arbitrary path for Exhibitor to dump its zk config to.
-exhibitor_zk_path: /dcos
-
-# More garbage collection, don't change unless you're advanced.
-#gc_delay: '2days'
-
-# We default master discovery to a static list of masters. Advanced options
-# are 'keepalived' and 'cloud_dynamic'
-master_discovery: static
-
-# Since we're defaulting to master list, we pass that static list of masters.
-# Recommended deployment as at least 3 masters and a maximum of 5. It's not
-# Recommended that you deploy even number of masters.
-master_list: '["10.33.2.21"]'
-
-# This is the list of upstream DNS resolvers that Mesos DNS will use.
-resolvers: '["8.8.8.8", "8.8.4.4"]'
-
-# Default roles, do not change unless you're advanced.
-#roles: slave_public
-
-# Default weights, do not change unless you're advanced.
-#weights: slave_public=1
-""")
 
 log = logging.getLogger(__name__)
 
 
 def do_configure(real_config):
-    # TODO(malnick) we're setting bootstrap to our foo bootstrap
-    # for demo purposes only
-    # os.environ['BOOTSTRAP_ID'] = 'e2ade83b3197150980714635c6752bc9ece1e0ca'
-    # os.environ['CHANNEL_NAME'] = 'testing/continuous'
-
     gen_options = gen.get_options_object()
     gen_options.output_dir = SERVE_DIR
     gen_options.assume_defaults = True
@@ -84,12 +31,6 @@ def do_configure(real_config):
 
 
 def do_onprem(gen_options, provider_module, mixins, genconf_config):
-    # if 'CHANNEL_NAME' in os.environ:
-    #    channel_name = os.environ['CHANNEL_NAME']
-    # else:
-    #    log.error("CHANNEL_NAME must be set in environment to run.")
-    #    return
-
     if 'BOOTSTRAP_ID' in os.environ:
         bootstrap_id = os.environ['BOOTSTRAP_ID']
     else:
@@ -98,7 +39,6 @@ def do_onprem(gen_options, provider_module, mixins, genconf_config):
 
     arguments = {
         'ip_detect_filename': IP_DETECT_PATH,
-        # 'channel_name': channel_name,
         'bootstrap_id': bootstrap_id,
         'provider': 'onprem'}
 

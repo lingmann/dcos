96c5c0d0b708bff2a4ae13e333eca6acf686173a
Jeremy Lingmann <jeremy@mesosphere.io>
2016-01-30 14:07:39 -0800

    Generate ARM templates for DCOS and ACS
    * Generate ARM templates for DCOS on Azure and DCOS for Azure Container
      Service. Since each template uses different variable names, we inject
      the appropriate ARM expression as user arguments when generating the
      cloud config, instead of using calc.py.
    * Add variable masterVMNic to the ACS template, since it is not possible
      to nest concat() functions with our in-line ARM expressions when we
      render the final template. The number of masterVMNic entries sets the
      upper limit to how many master nodes we can support with a template
      (currently 7).
    * Remove masterCount parameter from the ACS template and set
      automatically to the derived `num_masters` value as set by the gen library.
      The derived value is based on the number of elements in `master_list`.

--- gen/azure/calc.py
+++ gen/azure/calc.py
@@ -4,11 +4,6 @@ entry = {
         'ip_detect_filename': 'scripts/ip-detect/azure.sh',
         'master_discovery': 'static',
         'exhibitor_storage_backend': 'azure',
-        'exhibitor_azure_prefix': "[[[variables('uniqueName')]]]",
-        'exhibitor_azure_account_name': "[[[variables('storageAccountName')]]]",
-        'exhibitor_azure_account_key': "[[[listKeys(resourceId('Microsoft.Storage/storageAccounts', variables('storageAccountName')), '2015-05-01-preview').key1]]]",  # noqa
-        'exhibitor_address': "[[[reference('masterNodeNic0').ipConfigurations[0].properties.privateIPAddress]]]",
-        'cluster_name': "[[[variables('uniqueName')]]]",
         'master_cloud_config': '{{ master_cloud_config }}',
         'slave_cloud_config': '{{ slave_cloud_config }}',
         'slave_public_cloud_config': '{{ slave_public_cloud_config }}'

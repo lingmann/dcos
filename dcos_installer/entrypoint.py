252d8d85388a9acae49b2442ae0efcad52c787c3
Michael Ellenburg <michael.ellenburg@mesosphere.com>
2016-01-31 00:11:17 -0800

    Remove unecessary errors

--- dcos_installer/entrypoint.py
+++ dcos_installer/entrypoint.py
@@ -6,12 +6,6 @@ from dcos_installer import DcosInstaller
 
 
 def main():
-    channel = os.getenv("CHANNEL_NAME", None)
-    bootstrap_id = os.getenv("BOOTSTRAP_ID", None)
-    if channel is None:
-        exit("CHANNEL_NAME must be set in environment!")
-    if bootstrap_id is None:
-        exit("BOOTSTRAP_ID must be set in environment!")
     DcosInstaller(sys.argv[1:])
 
 if __name__ == '__main__':

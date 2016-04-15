55b57d25a4221c699fd1210c37eb6b65fb96bf45
Cody Maloney <cmaloney@theoreticalchaos.com>
2016-04-15 00:38:01 -0700

    Update to 1.7.0

--- gen/calc.py
+++ gen/calc.py
@@ -322,7 +322,7 @@ entry = {
         'dcos_image_commit': calulate_dcos_image_commit,
         'ip_detect_contents': calculate_ip_detect_contents,
         'mesos_dns_resolvers_str': calculate_mesos_dns_resolvers_str,
-        'dcos_version': '1.7-dev',
+        'dcos_version': '1.7.0',
         'dcos_gen_resolvconf_search_str': calculate_gen_resolvconf_search,
         'curly_pound': '{#',
         'cluster_packages': calculate_cluster_packages,
